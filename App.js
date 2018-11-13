1a94e50ffc75a856e10f5d34cbddc472e27056fd
muhammadumairmoosani <muhammadumairmoosani@gmail.com>
2018-11-13 20:04:39 +0500

    Partially implemented facebook authentication

--- App.js
+++ App.js
@@ -8,6 +8,7 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import RNAccountKit from 'react-native-facebook-account-kit'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -15,9 +16,42 @@ const instructions = Platform.select({
     'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
+RNAccountKit.loginWithPhone()
+  .then((token) => {
+    if (!token) {
+      console.log('Login cancelled')
+    } else {
+      console.log(`Logged with phone. Token: ${token}`)
+    }
+  })
 
-type Props = {};
-export default class App extends Component<Props> {
+
+
+// Logouts the user, so getCurrentAccessToken() will retrieve null
+RNAccountKit.logout()
+  .then(() => {
+    console.log('Logged out')
+  })
+
+// Retrieves the logged user access token, if any user is logged
+RNAccountKit.getCurrentAccessToken()
+  .then((token) => {
+    console.log(`Current access token: ${token}`)
+  })
+
+// Retrieves the logged user account info, if any user is logged
+RNAccountKit.getCurrentAccount()
+  .then((account) => {
+    console.log(`Current account: ${account}`)
+  })
+
+// Configures the SDK with some options
+RNAccountKit.configure({
+  responseType: 'token'|'code' // 'token' by default,
+})
+
+
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
